2135c8e5d09a5af8a48be2114dc734ad55ecc504
agnihotri-anxh <your-email@example.com>
2026-01-16 18:22:18 +0530

    commiting changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  skipMiddlewareValidation: true,
+  staticPageGenerationTimeout: 120,
   images: {
     remotePatterns: [
       {
